36ad47d248fd781a302808a114fd474bd0bff5bf
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2022-01-23 23:45:33 +0530

    Completed v1

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Elements from the page
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const btnReset = document.querySelector('.btn--new');
+const btnNew = document.querySelector('.btn--new');
+
+const diceEl = document.querySelector('.dice');
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -9,46 +14,80 @@ const current1El = document.getElementById('current--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 
-const diceEl = document.querySelector('.dice');
-
 // Initial State
-let score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores;
+let currentScore;
+let activePlayer;
+let playing;
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+function init() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  diceEl.classList.add('hidden');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+}
+
+init();
 
 // Utility Functions
 const randomDiceRoll = () => Math.ceil(Math.random() * 6);
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 };
 
 // Event Listeners
 btnRoll.addEventListener('click', function () {
-  // 1. Get a random dice roll
-  const dice = randomDiceRoll();
-  // 2. Check if dice roll is 1 or not
-  if (dice !== 1) {
-    currentScore += dice;
+  if (playing) {
+    // 1. Get a random dice roll
+    const dice = randomDiceRoll();
+
     diceEl.classList.remove('hidden');
     diceEl.src = `./Images/dice-${dice}.png`;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    switchPlayer();
+    // 2. Check if dice roll is 1 or not
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  console.log('Holding');
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      return;
+    } else {
+      switchPlayer();
+    }
+  }
 });
 
-btnReset.addEventListener('click', function () {
-  console.log('New Game');
-});
+btnNew.addEventListener('click', init);
